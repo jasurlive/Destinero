8c13fc5401006a452e9c3ed2c04c1fd85f40bd8d
jasurgraduate <jasur@graduate.org>
2025-04-10 00:05:46 +0900

    pop up window fix

--- src/add/tools/PopUp.tsx
+++ src/add/tools/PopUp.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import { Marker, Popup, MarkerProps } from 'react-leaflet';
-import '../css/popup.css';
-import { FaSpinner } from 'react-icons/fa';
-import { getCountryFlag } from './Flags';
-import L from 'leaflet';
-import ReactDOMServer from 'react-dom/server';
+import { useState } from "react";
+import { Marker, Popup, MarkerProps } from "react-leaflet";
+import "../css/popup.css";
+import { FaSpinner } from "react-icons/fa";
+import { getCountryFlag } from "./Flags";
+import L from "leaflet";
+import ReactDOMServer from "react-dom/server";
 
 interface Place {
   type: string;
@@ -12,6 +12,7 @@ interface Place {
   name: string;
   imageLink?: string;
   icon: React.ReactElement;
+  countryCode?: string;
 }
 
 interface CreatePopupProps {
@@ -32,7 +33,14 @@ const CustomMarker: React.FC<MarkerProps> = (props) => {
   return <Marker {...props} />;
 };
 
-const CreatePopup: React.FC<CreatePopupProps> = ({ place, mapRef, handleCopyClick, copySuccess, onPlaceClick, locationDetails }) => {
+const CreatePopup: React.FC<CreatePopupProps> = ({
+  place,
+  mapRef,
+  handleCopyClick,
+  copySuccess,
+  onPlaceClick,
+  locationDetails,
+}) => {
   const [imageLoaded, setImageLoaded] = useState(false);
 
   const handleImageLoad = () => {
@@ -47,43 +55,58 @@ const CreatePopup: React.FC<CreatePopupProps> = ({ place, mapRef, handleCopyClic
 
   return (
     <CustomMarker
-      key={`${place.type}-${place.coords.join(',')}`}
+      key={`${place.type}-${place.coords.join(",")}`}
       position={place.coords}
       icon={L.divIcon({
         html: ReactDOMServer.renderToString(place.icon),
-        className: 'custom-icon',
+        className: "custom-icon",
       })}
     >
       <Popup>
         <div className="pop-up-container">
-          <h2 className="place-name">{place.name}</h2>
           {locationDetails && (
             <>
+              <h2 className="place-name">Clicked location 📌</h2>
               <p>{locationDetails.placeName}</p>
-              <p className="city-name-popup">{locationDetails.city}, {locationDetails.country} {getCountryFlag(locationDetails.countryCode)}</p>
+              <p className="city-name-popup">
+                {locationDetails.city || "Unknown city"},{" "}
+                {locationDetails.country?.trim() || "Unknown country"}{" "}
+                {getCountryFlag(locationDetails.countryCode || "")}
+              </p>
               <p>{`Latitude: ${place.coords[0]}, Longitude: ${place.coords[1]}`}</p>
               <button
-                className={`copy-button ${copySuccess ? 'copied' : ''}`}
+                className={`copy-button ${copySuccess ? "copied" : ""}`}
                 onClick={handleCopy}
               >
-                {copySuccess ? 'Copied 😁' : 'Copy Coords 🏌🏻‍♂️'}
+                {copySuccess ? "Copied 😁" : "Copy Coords 🏌🏻‍♂️"}
               </button>
             </>
           )}
-          {place.type === 'current' && (
-            <a href="https://jasurlive.uz" target="_blank" rel="noopener noreferrer">
-            </a>
+          {place.type === "current" && (
+            <a
+              href="https://jasurlive.uz"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
           )}
           {place.imageLink && (
-            <a href="https://jasurlive.uz" target="_blank" rel="noopener noreferrer">
-              {!imageLoaded && <FaSpinner className="spinner-popup" />}
-              <img
-                src={place.imageLink}
-                alt={place.name}
-                className={`place-image ${imageLoaded ? 'loaded' : ''}`}
-                onLoad={handleImageLoad}
-              />
-            </a>
+            <>
+              {" "}
+              <h2 className="place-name">{place.name}</h2>
+              <a
+                href="https://jasurlive.uz"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {!imageLoaded && <FaSpinner className="spinner-popup" />}
+                <img
+                  src={place.imageLink}
+                  alt={place.name}
+                  className={`place-image ${imageLoaded ? "loaded" : ""}`}
+                  onLoad={handleImageLoad}
+                />
+              </a>
+            </>
           )}
         </div>
       </Popup>
